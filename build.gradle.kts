d347d19366706b20f807dd298242eb5bc0229ba2
Spageetus <blakehellenbrand@gmail.com>
2025-01-19 18:03:40 -0500

    Connected Application to Firebase

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,10 @@
-// Top-level build file where you can add configuration options common to all subprojects/modules.
-
-
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
     id("com.android.application") version "8.8.0-alpha05" apply false
+
+    // Adds the dependency for the Google services Gradle plugin
+    id("com.google.gms.google-services") version "4.4.2" apply false
+
     id("com.android.library") version "8.8.0-alpha05" apply false
     id("org.jetbrains.kotlin.android") version "1.9.0" apply false
     id("com.google.gms.google-services") version "4.3.15" apply false
