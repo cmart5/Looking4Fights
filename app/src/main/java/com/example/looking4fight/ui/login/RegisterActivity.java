0f39525a671debbbb40ad95f73f826f88612bb86
Spageetus <blakehellenbrand@gmail.com>
2025-01-30 18:17:28 -0500

    Added Registration page

---
+++ app/src/main/java/com/example/looking4fight/ui/login/RegisterActivity.java
@@ -0,0 +1,105 @@
+package com.example.looking4fight.ui.login;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import com.example.looking4fight.MainActivity;
+import com.example.looking4fight.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.textfield.TextInputLayout;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import static android.content.ContentValues.TAG;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
+
+    TextInputLayout editTextEmail, editTextPassword, editTextConfirmPassword;
+    Button buttonReg;
+    @Override
+    public void onStart() {
+        //If user is already signed in, automatically sign in
+        super.onStart();
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if(currentUser != null){
+            Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+            startActivity(intent);
+            finish();
+        }
+    }
+
+    //@Override
+    protected void OnCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        mAuth = FirebaseAuth.getInstance();
+        setContentView(R.layout.activity_register);
+        editTextEmail = findViewById(R.id.username);
+        editTextPassword = findViewById(R.id.password);
+        editTextPassword = findViewById(R.id.password_confirm);
+        buttonReg = findViewById(R.id.btn_register);
+
+        buttonReg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email, password, confirmPassword;
+                email = String.valueOf(editTextEmail.getEditText());
+                password = String.valueOf(editTextPassword.getEditText());
+                confirmPassword = String.valueOf(editTextConfirmPassword.getEditText());
+
+                if(TextUtils.isEmpty(email))
+                {
+                    Toast.makeText(RegisterActivity.this, "Enter email", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                if(TextUtils.isEmpty(password))
+                {
+                    Toast.makeText(RegisterActivity.this, "Enter password", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                if (TextUtils.isEmpty(confirmPassword)) {
+                    Toast.makeText(RegisterActivity.this, "Confirm password", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                if(!TextUtils.equals(password, confirmPassword))
+                {
+                    Toast.makeText(RegisterActivity.this, "Passwords do not match", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                //creates new user
+                mAuth.createUserWithEmailAndPassword(email, password)
+                        .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if (task.isSuccessful()) {
+                                    Toast.makeText(RegisterActivity.this, "Account created.",
+                                            Toast.LENGTH_SHORT).show();
+
+                                    //goes back to Login Activity
+                                    Intent intent = new Intent(RegisterActivity.this, LoginActivity.class);
+                                    startActivity(intent);
+                                    finish();
+
+                                } else {
+                                    // If sign in fails, display a message to the user.
+                                    Toast.makeText(RegisterActivity.this, "Authentication failed.",
+                                            Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                        });
+            }
+        });
+
+    }
+
+}
