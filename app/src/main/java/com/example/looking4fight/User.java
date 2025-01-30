09af4a4c7a4856089d9bf5b129787028993ef443
Andrew Riker <AMRiker@student.fullsail.edu>
2025-01-29 22:00:46 -0500

    Add User and UserSearch Java classes

---
+++ app/src/main/java/com/example/looking4fight/User.java
@@ -0,0 +1,19 @@
+package com.example.looking4fight;
+
+public class User {
+    private String name;
+    private int id;
+
+    public User(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getId() {
+        return id;
+    }
+}
