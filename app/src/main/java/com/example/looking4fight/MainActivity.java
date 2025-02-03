0fc271efaff1361d656e0e7303862e32bb590d59
Spageetus <blakehellenbrand@gmail.com>
2025-02-02 19:14:56 -0500

    Added temporary Log Out button

--- app/src/main/java/com/example/looking4fight/MainActivity.java
+++ app/src/main/java/com/example/looking4fight/MainActivity.java
@@ -1,13 +1,36 @@
 package com.example.looking4fight;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
+import com.example.looking4fight.ui.login.LoginActivity;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button buttonLogout;
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mAuth = FirebaseAuth.getInstance();
+        buttonLogout = findViewById(R.id.btn_logout);
+
+        buttonLogout.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v) {
+                mAuth.signOut();
+                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+
     }
 }
