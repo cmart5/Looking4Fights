265fef13ff6ec2aaa424cc0956ec90f5de800b36
Andrew Riker <AMRiker@student.fullsail.edu>
2025-02-13 15:18:51 -0500

    Begin Fuzzy Search Changes (Levenshtein Distance algorithm)

--- app/src/main/java/com/example/looking4fight/UserSearch.java
+++ app/src/main/java/com/example/looking4fight/UserSearch.java
@@ -21,6 +21,29 @@ public class UserSearch {
         users.add(new User(4, "Emily Davis", LocalDate.of(2024, 2, 5)));
     }
 
+    // Levenshtein Distance Algorithm
+    private int levenshteinDistance(String s1, String s2) {
+        int[][] dp = new int[s1.length() + 1][s2.length() + 1];
+
+        for (int i = 0; i <= s1.length(); i++) {
+            for (int j = 0; j <= s2.length(); j++) {
+                if (i == 0) {
+                    dp[i][j] = j;
+                } else if (j == 0) {
+                    dp[i][j] = i;
+                } else {
+                    int cost = (s1.charAt(i - 1) == s2.charAt(j - 1)) ? 0 : 1;
+                    dp[i][j] = Math.min(Math.min(
+                                    dp[i - 1][j] + 1,        // Deletion
+                                    dp[i][j - 1] + 1),       // Insertion
+                            dp[i - 1][j - 1] + cost  // Substitution
+                    );
+                }
+            }
+        }
+        return dp[s1.length()][s2.length()];
+    }
+
     // Search function to find users by name with optional date filter
     @RequiresApi(api = Build.VERSION_CODES.O)
     public List<User> searchUsers(String query, LocalDate datePosted) {
