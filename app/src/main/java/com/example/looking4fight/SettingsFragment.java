96f759365e7ac0f7a243adcd2d346ed4ca5e040f
Spageetus <blakehellenbrand@gmail.com>
2025-02-22 18:23:16 -0500

    added sign out button to settings screen

--- app/src/main/java/com/example/looking4fight/SettingsFragment.java
+++ app/src/main/java/com/example/looking4fight/SettingsFragment.java
@@ -1,13 +1,15 @@
 package com.example.looking4fight;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import com.example.looking4fight.ui.login.LoginActivity;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class SettingsFragment extends Fragment {
    
@@ -15,10 +17,19 @@ public class SettingsFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-
-
-        return inflater.inflate(R.layout.fragment_settings, container, false);
+        View v = inflater.inflate(R.layout.fragment_settings, container, false);
+        Button logoutButton = v.findViewById(R.id.btn_signout);
+        logoutButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //sign out and return to login page
+                FirebaseAuth.getInstance().signOut();
+                Intent intent = new Intent(getActivity(), LoginActivity.class);
+                startActivity(intent);
+                getActivity().finish();
+            }
+        });
+        return v;
     }
-
 
 }
