d73b6335f13e99e3f999e8171ff0a9f6aad260f5
Spageetus <blakehellenbrand@gmail.com>
2025-03-01 15:35:32 -0500

    minor changes to make IntelliJ stop screaming at me

--- app/src/main/java/com/example/looking4fight/data/model/Event.java
+++ app/src/main/java/com/example/looking4fight/data/model/Event.java
@@ -1,10 +1,14 @@
 package com.example.looking4fight.data.model;
 
+import com.google.firebase.firestore.GeoPoint;
+
 public class Event {
     private String title;
     private String date;
     private String location;
     private String description;
+
+    private GeoPoint locationPoint;
 
     public Event() {
         // Empty constructor required for Firestore
@@ -19,6 +23,10 @@ public class Event {
 
     public String getTitle() { return title; }
     public String getDate() { return date; }
-    public String getLocation() { return location; }
+    //public String getLocation() { return location; } //removed to fix error
     public String getDescription() { return description; }
+
+    public GeoPoint getLocation() { return locationPoint; } //added to fix error
+
+    public void setLocation(GeoPoint locationPoint) { this.locationPoint = locationPoint; } //added to fix error
 }
